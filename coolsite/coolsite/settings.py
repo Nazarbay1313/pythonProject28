78d84e884786c31b13245b56256ccdca87f84ab8
Nazarbay Nazarbaev <nazik3110@gmail.com>
2024-02-24 14:45:13 +0200

    commit SMTP

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -154,7 +154,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'django.business.site@gmail.com'
-EMAIL_HOST_PASSWORD = 'wvqoekwgbrridvqj'
+EMAIL_HOST_PASSWORD = 'gfrgkdtsobbhrwhp'
 
 
 CACHES = {
